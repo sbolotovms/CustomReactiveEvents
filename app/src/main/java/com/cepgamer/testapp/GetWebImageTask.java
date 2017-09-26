1a40482cde2aa454d26f0fe2c5042bff97609a82
Sergei Bolotov <sbolotov@microsoft.com>
2017-09-25 18:58:14 -0700

    Added button handler

--- app/src/main/java/com/cepgamer/testapp/GetWebImageTask.java
+++ app/src/main/java/com/cepgamer/testapp/GetWebImageTask.java
@@ -1,34 +1,73 @@
 package com.cepgamer.testapp;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-public class GetWebImageTask extends AsyncTask<URL, Void, Bitmap> {
+public class GetWebImageTask extends AsyncTask<URL, Void, Bitmap>
+{
     private OnDownloadedCallback _onDownloadedCallback;
 
-    public interface OnDownloadedCallback {
+    public interface OnDownloadedCallback
+    {
         void result(Bitmap result);
     }
 
-    public GetWebImageTask(OnDownloadedCallback onDownloadedCallback) {
+    public GetWebImageTask(OnDownloadedCallback onDownloadedCallback)
+    {
 
         _onDownloadedCallback = onDownloadedCallback;
     }
 
     @Override
-    protected Bitmap doInBackground(URL... params) {
-        if (params.length > 0) {
+    protected Bitmap doInBackground(URL... params)
+    {
+        if (params.length > 0)
+        {
             URL url = params[0];
-            try {
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+            try
+            {
                 HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
+                inputStream = new BufferedInputStream(connection.getInputStream());
+                return BitmapFactory.decodeStream(inputStream);
 
-            } catch (Exception e) {
+            } catch (Exception e)
+            {
                 Log.e(GetWebImageTask.class.getName(), "Error getting image", e);
+            } finally
+            {
+                if (inputStream != null)
+                {
+                    try
+                    {
+                        inputStream.close();
+                    } catch (final IOException e)
+                    {
+                        // do nothing
+                    }
+                }
+
+                if (outputStream != null)
+                {
+                    try
+                    {
+                        inputStream.close();
+                    } catch (final IOException e)
+                    {
+                        // do nothing
+                    }
+                }
             }
         }
 
@@ -36,7 +75,8 @@ public class GetWebImageTask extends AsyncTask<URL, Void, Bitmap> {
     }
 
     @Override
-    protected void onPostExecute(Bitmap bitmap) {
+    protected void onPostExecute(Bitmap bitmap)
+    {
         _onDownloadedCallback.result(bitmap);
     }
 }
--- app/src/main/java/com/cepgamer/testapp/MainActivityLayoutActivity.java
+++ app/src/main/java/com/cepgamer/testapp/MainActivityLayoutActivity.java
@@ -1,5 +1,7 @@
 package com.cepgamer.testapp;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,62 +14,114 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivityLayoutActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener
+{
+
+    private Button _downloadButton;
+    private EditText _editText;
+    private ImageView _imageView;
+    private TextView _errorText;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity_layout);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
+ 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        _editText = (EditText) findViewById(R.id.image_link);
+        _imageView = (ImageView) findViewById(R.id.image);
+        _errorText = (TextView) findViewById(R.id.error_text);
+
+        _downloadButton = (Button) findViewById(R.id.download_button);
+        _downloadButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                try
+                {
+                    final URL imageUrl = new URL(_editText.getText().toString());
+
+                    new GetWebImageTask(new GetWebImageTask.OnDownloadedCallback()
+                    {
+                        @Override
+                        public void result(Bitmap result)
+                        {
+                            if (result != null)
+                            {
+                                _imageView.setImageBitmap(result);
+                                _errorText.setVisibility(View.GONE);
+                            }
+                            else
+                            {
+                                _imageView.setImageBitmap(null);
+                                _errorText.setVisibility(View.VISIBLE);
+                            }
+                        }
+                    }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, imageUrl);
+                } catch (final MalformedURLException e)
+                {
+                    Toast.makeText(MainActivityLayoutActivity.this, "URL you entered is invalid", Toast.LENGTH_LONG);
+                    _errorText.setVisibility(View.VISIBLE);
+                }
+            }
+        });
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        }
+        else
+        {
             super.onBackPressed();
         }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
@@ -76,21 +130,33 @@ public class MainActivityLayoutActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_camera)
+        {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        }
+        else if (id == R.id.nav_gallery)
+        {
 
-        } else if (id == R.id.nav_slideshow) {
+        }
+        else if (id == R.id.nav_slideshow)
+        {
 
-        } else if (id == R.id.nav_manage) {
+        }
+        else if (id == R.id.nav_manage)
+        {
 
-        } else if (id == R.id.nav_share) {
+        }
+        else if (id == R.id.nav_share)
+        {
 
-        } else if (id == R.id.nav_send) {
+        }
+        else if (id == R.id.nav_send)
+        {
 
         }
 
